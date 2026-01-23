7d6b922df21721baf758ac9e96f2bd07b268cb05
CM <imperfect.cm@gmail.com>
2026-01-23 11:31:18 +0800

    added favicon and google fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                "zalando-sans": ['var(--font-zalando-sans)', 'sans-serif'],
+                "stack-sans-notch": ['var(--font-stack-sans-notch)', 'sans-serif'],
+            },
+        },
+    },
+};
